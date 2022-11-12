8c8908e020cbd5731d0b7f2d6873d116abdf91d7
Nitesh Dewada <nitesh.dewada@go-upwards.com>
2022-11-12 16:31:11 +0530

    First Commit

---
+++ ApiKey.kt
@@ -0,0 +1,8 @@
+package com.androidready.gitdemo
+
+//this is secret api key
+
+//- enable vcs
+//        - add single file
+//        - use of git ignore with api key file
+//        - show git tab and show commit
